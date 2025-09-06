60771fa7d96c848d9085d8705c5158e85455458a
ramyaa-n <ramyan.0704@gmail.com>
2025-09-06 19:36:27 +0530

    Sample commit to test

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ const Home = () => {
     <div style={{ padding: '2rem' }}>
       <h1>Welcome to the Home Page</h1>
       <p>This is the main landing page of our application. From here, you can navigate to other sections.</p>
+      <p>This is the Demo Line that i have added to check my commit!!</p>
     </div>
   );
 };
