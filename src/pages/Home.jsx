f5a9c78e3b8a9dcdd4230b09e547f09f70c6181d
BellamDeva <bellamdeva98@gmail.com>
2025-09-07 07:59:47 +0530

    For testing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ const Home = () => {
       <h1>Welcome to the Home Page</h1>
       <p>This is the main landing page of our application. From here, you can navigate to other sections.</p>
       <p>This is the Demo Line that i have added to check my commit!!</p>
+      <p>This is other line.</p>
     </div>
   );
 };
